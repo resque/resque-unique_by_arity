71a190a9d13551f04d6d47bdf2c3d13ab1edd64a
Peter Boling <peter.boling@gmail.com>
2017-10-01 03:28:11 -0700

    Don't trigger warnings on default configuration initialization

--- lib/resque/unique_by_arity/configuration.rb
+++ lib/resque/unique_by_arity/configuration.rb
@@ -17,9 +17,12 @@ module Resque
         @unique_at_runtime = options.key?(:unique_at_runtime) ? options[:unique_at_runtime] : false
         @unique_in_queue = options.key?(:unique_in_queue) ? options[:unique_in_queue] : false
         @unique_across_queues = options.key?(:unique_across_queues) ? options[:unique_across_queues] : false
-        warn ":arity_for_uniqueness is set to #{@arity_for_uniqueness}, but no uniqueness enforcement was turned on [:unique_at_runtime, :unique_in_queue, :unique_across_queues]" unless @unique_at_runtime || @unique_in_queue || @unique_across_queues
-        warn ":lock_after_execution_period is set to #{@lock_after_execution_period}, but :unique_at_runtime is not set" if @lock_after_execution_period && !@unique_at_runtime
-        warn ":unique_in_queue and :unique_across_queues should not be set at the same time, as :unique_across_queues will always supercede :unique_in_queue" if @unique_in_queue && @unique_across_queues
+        # The default config initialization shouldn't trigger any warnings.
+        if options.keys.length > 0
+          warn ":arity_for_uniqueness is set to #{@arity_for_uniqueness}, but no uniqueness enforcement was turned on [:unique_at_runtime, :unique_in_queue, :unique_across_queues]" unless @unique_at_runtime || @unique_in_queue || @unique_across_queues
+          warn ":lock_after_execution_period is set to #{@lock_after_execution_period}, but :unique_at_runtime is not set" if @lock_after_execution_period && !@unique_at_runtime
+          warn ":unique_in_queue and :unique_across_queues should not be set at the same time, as :unique_across_queues will always supercede :unique_in_queue" if @unique_in_queue && @unique_across_queues
+        end
       end
       def to_hash
         {
