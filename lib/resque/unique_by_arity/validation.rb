d762876832f1842ea18f9411b93eab8c96f63ae8
Peter Boling <peter.boling@gmail.com>
2017-11-02 03:19:14 -0700

    Remove verbose output

--- lib/resque/unique_by_arity/validation.rb
+++ lib/resque/unique_by_arity/validation.rb
@@ -2,7 +2,6 @@ module Resque
   module UniqueByArity
     module Validation
       def perform(*_)
-        puts "performed!"
         uniq_config.validate_arity(self.to_s, self.method(:perform).super_method) if uniq_config.arity_validation
         super
       end
