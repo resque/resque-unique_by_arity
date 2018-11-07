6c503fbd11714884f27f1f67fb10b227cdde1a5f
Peter Boling <peter.boling@gmail.com>
2018-11-07 00:22:58 -0800

    require rspec/block_is_expected

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,17 @@
-require "bundler/setup"
+require 'bundler/setup'
 
 require 'byebug' if RbConfig::CONFIG['RUBY_INSTALL_NAME'] == 'ruby'
+require 'rspec/block_is_expected'
 
 require 'simplecov'
 SimpleCov.start
 
 # This gem
-require "resque/unique_by_arity"
+require 'resque/unique_by_arity'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
