1bb6c09dbba01c6f1a882ba37713efbdd127216b
Peter H. Boling <peter.boling@gmail.com>
2026-08-01 15:32:13 -0600

    🔖 Bump gem version

--- lib/resque/unique_by_arity/version.rb
+++ lib/resque/unique_by_arity/version.rb
@@ -5,7 +5,7 @@ module Resque
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = "3.0.2"
+      VERSION = "3.0.3"
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
